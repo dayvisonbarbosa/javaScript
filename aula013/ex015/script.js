79a4a9c37f573110ccf64ca2697854f9c8d8b0cb
Dayvison Barbosa dos Santos <dayvisonbarbosadossantos@gmail.com>
2026-08-24 18:57:20 -0300

    updt

--- aula013/ex015/script.js
+++ aula013/ex015/script.js
@@ -8,15 +8,15 @@ function carregar() {
         jshora.innerHTML = `Agora são ${hora} horas`
         if (hora >= 0 && hora < 12) {
             // Bom dia
-            jsimg.src = ("manha.png")
+            jsimg.src = "manha.png"
             document.body.style.background = "#e2cd9f" 
         } else if (hora >= 12 && hora < 18) {
             // Boa tarde
-            jsimg.src = ("tarde.png")
+            jsimg.src = "tarde.png"
             document.body.style.background = "#b9846f"
         } else {
             // Boa noite
-            jsimg.src = ("noite.png")
+            jsimg.src = "noite.png"
             document.body.style.background = "#515154"
         }
 
