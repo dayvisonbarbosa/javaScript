d4328cf9132a684e7201ce0ba5ad52fe25634096
Dayvison Barbosa dos Santos <dayvisonbarbosadossantos@gmail.com>
2026-08-31 18:01:12 -0300

    ex015

--- aula013/ex014/script.js
+++ aula013/ex014/script.js
@@ -1,8 +1,55 @@
 function verificar() {
-    var data, jsano
+    var data, jsano, formano, jsresultado, jssex, jsidade, genero, jsimg
 
-    data = new Date
-    jsano = data.getFullYear
+    data = new Date()
+    jsano = data.getFullYear()
+    formano = document.getElementById(`ano`)
+    jsresultado = document.getElementById(`resultado`)
+    if (formano.value == 0 || formano.value > jsano) {
+        window.alert(`[ERRO] Verifique os dados e tente novamnte!`)
+    } else {
+        jssex = document.getElementsByName(`sexo`)
+        jsidade = jsano - Number(formano.value)
+        genero = ""
+        jsimg = document.createElement(`img`)
+        jsimg.setAttribute(`id`, `foto`)
+        if (jssex[0].checked) {
+            genero = "Homem"
+            if (jsidade >= 0 && jsidade < 10) {
+                // criança
+                jsimg.setAttribute(`src`, `imagens/bebemas.png`)
+            } else if (jsidade < 21) {
+                // jovem
+                jsimg.setAttribute(`src`, `imagens/jovemmas.png`)
+            } else if (jsidade < 50) {
+                // adulto
+                jsimg.setAttribute(`src`, `imagens/adulto.png`)
+            } else {
+                // idoso
+                jsimg.setAttribute(`src`, `imagens/idosomas.png`)
+            }
+        } else if (jssex[1].checked) {
+            genero = "Mulher"
+            if (jsidade >= 0 && jsidade < 10) {
+                // criança
+                jsimg.setAttribute(`src`, `imagens/bebefem.png`)
+            } else if (jsidade < 21) {
+                // jovem
+                jsimg.setAttribute(`src`, `imagens/jovemfem.png`)
+            } else if (jsidade < 50) {
+                // adulto
+                jsimg.setAttribute(`src`, `imagens/adulta.png`)
+            } else {
+                // idoso
+                jsimg.setAttribute(`src`, `imagens/idosofem.png`)
+            }
+        }
+        jsresultado.style.textAlign = "center"
+        jsresultado.innerHTML = `<p>Detectamos ${genero} com ${jsidade} anos.</p>`
+        jsresultado.appendChild(jsimg)
+    } 
+
+    
 }
 
 
