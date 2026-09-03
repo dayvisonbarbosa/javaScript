f05e2610ac8c4fef605fd05cf887110085fabf9a
Dayvison Barbosa dos Santos <dayvisonbarbosadossantos@gmail.com>
2026-09-03 18:16:24 -0300

    update

--- aula016/ex016/script.js
+++ aula016/ex016/script.js
@@ -1,1 +1,25 @@
+function contar() {
+    var ligacaoin, jsinicio, ligacaofim, jsfim, ligacaopasso, jspasso, jsresultado, c
 
+    ligacaoin = document.getElementById("inicio")
+    ligacaofim = document.getElementById("fim")
+    ligacaopasso = document.getElementById("passo")
+    jsresultado = document.getElementById("resultado")
+
+    if (ligacaoin.value === "" || ligacaofim.value === "" || ligacaopasso.value === "") {
+        jsresultado.innerHTML = "Não foi possível executar a conta"
+    } else {
+        jsinicio = Number(ligacaoin.value)
+        jsfim = Number(ligacaofim.value)
+        jspasso = Number(ligacaopasso.value)
+
+        jsresultado.innerHTML = `${jsinicio} 👉 `
+        c = jsinicio
+
+        while (c < jsfim) {
+            c = c + jspasso
+            jsresultado.innerHTML += `${c} 👉`
+        }
+        jsresultado.innerHTML += `🏁`
+    }
+}
