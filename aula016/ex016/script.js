99de57ba5f948c258ddcbef37049554b1316c088
Dayvison Barbosa dos Santos <dayvisonbarbosadossantos@gmail.com>
2026-09-07 16:49:37 -0300

    teste

--- aula016/ex016/script.js
+++ aula016/ex016/script.js
@@ -9,7 +9,7 @@ function contar() {
     if (ligacaoin.value === "" || ligacaofim.value === "" || ligacaopasso.value === "") {
         jsresultado.innerHTML = "Não foi possível executar a conta"
     } else if (ligacaopasso.value === "0" ) {
-        window.alert("Passo inválido")
+        jsresultado.innerHTML = "Passo inválido"
     } else {
         jsinicio = Number(ligacaoin.value)
         jsfim = Number(ligacaofim.value)
