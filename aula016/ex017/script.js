cc304781eb94794ed55910be1ca5493892816126
Dayvison Barbosa dos Santos <dayvisonbarbosadossantos@gmail.com>
2026-09-08 17:15:04 -0300

    Update script.js

--- aula016/ex017/script.js
+++ aula016/ex017/script.js
@@ -6,11 +6,16 @@ function calcular() {
     jsresultado = document.getElementById("resultado")
 
     contador = 0
-    jsresultado.innerHTML += `<option>${jsnumero} x 0 = 0</option>`
-    while (contador < 10) {
-        contador++
-        multiplicado = jsnumero * contador
-        jsresultado.innerHTML += `<option>${jsnumero} x ${contador} = ${multiplicado}</option>`
+    if (lignumero.value === "") {
+        window.alert("Por favor digite um número.")
+    } else { 
+        jsresultado.innerHTML = ""
+        jsresultado.innerHTML += `<option>${jsnumero} x 0 = 0</option>`
+        while (contador < 10) {
+            contador++
+            multiplicado = jsnumero * contador
+            jsresultado.innerHTML += `<option>${jsnumero} x ${contador} = ${multiplicado}</option>`
+        }
     }
 }
 
