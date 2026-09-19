d90422cb2a0e2e6558306cc93f5c78fd9f1c6dd6
Dayvison Barbosa dos Santos <dayvisonbarbosadossantos@gmail.com>
2026-09-19 16:21:43 -0300

    updt

--- exerciciosarray/ex05/script.js
+++ exerciciosarray/ex05/script.js
@@ -1,5 +1,23 @@
+
+let jsnumero = document.getElementById("num")
+let jsselect = document.getElementById("select")
+let vet = []
+
+
 function adicionar() {
-    let jsnumero = document.getElementById("num")
-    let jsselect = document.getElementById("select")
-    jsselect.innerHTML += `<option>${jsnumero.value}</option>`
+    let valores = Number(jsnumero.value)
+    vet.push(valores)
+    jsselect.innerHTML += `
+        <option>${jsnumero.value}</option>
+    `
+}
+
+function finalizar() {
+    let soma = 0
+
+    for (let pos = 0; pos < vet.length; pos++) {
+        soma += vet[pos]
+    }
+
+    window.alert(`${soma}`)
 }
